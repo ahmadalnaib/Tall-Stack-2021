189803b309bf3c191300c0090668e4514846cc4f
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-10 20:29:27 +0100

    add model mfc

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,20 +17,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+
         'email',
-        'password',
+
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+
 
     /**
      * The attributes that should be cast to native types.
