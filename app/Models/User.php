202290938a4004ba11eb0c3db69ca4885aabcb52
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-12 15:19:59 +0100

    add breez

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,12 +17,16 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-
+          'name',
         'email',
+        'password'
 
     ];
 
-
+protected  $hidden =[
+  'password',
+  'remember_token'
+];
 
     /**
      * The attributes that should be cast to native types.
